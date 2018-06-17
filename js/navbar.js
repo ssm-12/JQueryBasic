32107274d1d63e65616667378716ce36f1b1aa91
Souvik Roy <souvikroy.it@gmail.com>
2018-06-17 15:55:19 +0530

    Final Submit

--- js/navbar.js
+++ js/navbar.js
@@ -11,4 +11,22 @@ $(document).ready(function(){
 		}
 	});
 
+	$("a").click(function(e){
+		e.preventDefault();
+	});
+
+	$('#dropDownLink').click(function() {
+	    $('#dropDownContainer').css({
+	        left: $(this).offset().left + 'px',
+	        top: ($(this).offset().top + $(this).height()) + 20 + 'px'
+	    }).toggle(100);
+	});
+
+	$(document).click(function(e){
+		if(!$(e.target).is("#dropDownLink"))
+		{
+			$("#dropDownContainer").hide(100);
+		}
+	});
+
 });
